53663b8ab88cdbffeaedb9a2ca8009ebb5b265a3
Harutyunyan-Erik <erik2698@gmail.com>
2024-07-29 22:47:44 +0400

    checked error in mainLayout

--- src/view/layouts/MainLayout/index.js
+++ src/view/layouts/MainLayout/index.js
@@ -1,17 +1,18 @@
-import Header from '../../components/global/Header';
 import { Outlet } from 'react-router-dom';
+
+import Header from '../../components/global/header';
 import './index.css';
 
 const MainLayout = () => {
-    return(
-        <div className="main_layout_container">
-            <Header />
+    return (
+      <div className="main_layout_container">
+        <Header />
 
-            <main>
-                <Outlet />
-            </main>
-        </div>
+        <main>
+          <Outlet />
+        </main>
+      </div>
     )
 };
-
+  
 export default MainLayout;
